b8fb99899d2181f584b09a01789ea419d33e789f
ngyh-1002 <01049200297aa@gmail.com>
2025-08-12 10:43:34 +0900

    yolo를 톨한 객체검출
    yolo를 톨한 객체검출

--- src/yolo_image.py
+++ src/yolo_image.py
@@ -2,6 +2,16 @@ from ultralytics import YOLO
 
 model = YOLO('yolo11n.pt')
 
-results = model('https://ultralytics.com/images/bus.jpg')
+# results = model('https://ultralytics.com/images/bus.jpg')
 
-results[0].show()
+test_images = [
+    'https://ultralytics.com/images/zidan.jpg'
+    'https://ultralytics.com/images/bus.jpg'
+]
+
+for img in test_images:
+    results = model(img)
+    print(f"검출된 객체 수 : {len(results[0].boxes)}")
+    
+print(f"검출된 객체 수 : {len(results[1].boxes)}")
+results[1].show()
